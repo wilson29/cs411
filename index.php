47e2a8530e5fa4f9dc8d3b571e8b13c5f5fc9ee8
root <1upgreenshroom@gmail.com>
2012-03-13 22:01:12 -0500

    connected to database

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?
-
+$con = mysql_connect('localhost','scott');
+@mysql_select_db('test') or die( "Unable to select database");
+$result = mysql_query("SELECT * FROM GAMES");
+while($game = mysql_fetch_array($result)){
+  print($game['TITLE']." is rated ".$game['RATING'].'<br>');
+}
 ?>
